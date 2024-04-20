ac42d8286c1461c262c0940f278aa71e45171301
Abuelyazeed <abuelyazeed175@gmail.com>
2024-04-20 14:07:20 +0200

    added items model and added items to index page

--- item/models.py
+++ item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,21 @@ class Category(models.Model):
     class Meta:
         ordering = ("name",)
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name="items", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='items_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    # related name get all items belonging to a specific user
+    # on delete cascade -> if a user is deleted all of his items will also be deleted
+    created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
